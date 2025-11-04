a7761c5372d6e9df780b91a06c76ba78bdded95c
Melikadezfoli-Tech <melikadezfoli@gmail.com>
2025-11-04 17:29:00 +0100

    update

---
+++ main.js
@@ -0,0 +1,62 @@
+
+//VARIABLES
+//CAPTURAR ELEMENTOS DEL DOM
+const formulario = document.querySelector("#formulario")
+const boton = document.querySelector('#solDato');
+const parrafo = document.querySelector('#cuadro');
+const asincrono=document.querySelector('#asincrono')
+
+
+const api = { id:1,
+              nombre:'pepito',
+              edad:'20',
+              email:'pepito@gmail.com'             
+            }
+/* const api =[]
+ */
+/*EVENTOS*/
+formulario.addEventListener('submit',(evento)=>{
+    getUsuario(api)
+    .then((resultado)=>console.log(`el usuario ${resultado.nombre} tiene el correo ${resultado.email}`,mostrarPantalla(resultado.nombre,resultado.email)))
+    .catch((error)=>{console.log(error)});
+    evento.preventDefault();
+})   
+    
+
+
+// FUNCIONES    
+
+const mostrarPantalla=(nombre,correo)=>{
+    
+    return parrafo.textContent=`${nombre} con el correo ${correo}`
+} 
+const mostrarPantalla2=(nombre,correo)=>{
+    return asincrono.textContent=`info desde asincrona: el usuario ${nombre} con correo ${correo}`
+}
+
+const getUsuario =()=>{
+    const promise = new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if(api.id>0){
+                resolve(api)
+            }else{
+                reject("la hemos liado ")
+            }
+        }, 2000);
+    })
+    return promise
+}
+//INVOCACION A LAS FUNCIONES
+
+
+const asincornica = async (api) =>{
+    try{
+    return api
+    }catch (error){
+        throw error
+    }
+}
+
+asincornica(api)
+.then((resultado)=>mostrarPantalla(resultado.nombre,resultado.email))
+.catch((error)=>console.log(error)) 
